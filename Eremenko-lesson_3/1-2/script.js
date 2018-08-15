0801a16aa34888cacfc172c325c513808a73b157
Сергей <serserch@yandex.ru>
2018-08-15 15:29:42 +0300

    let

--- Eremenko-lesson_3/1-2/script.js
+++ Eremenko-lesson_3/1-2/script.js
@@ -1,4 +1,4 @@
 var text = "'Lorem ipsum dol'or' sit 'amet', conse'ctetur 'adipisicing elit'.";
-const reg = /\B'|'\B/g;
+var reg = /\B'|'\B/g;
 var res = text.replace(reg, '"');
 console.log(res);
--- Eremenko-lesson_3/3/script.js
+++ Eremenko-lesson_3/3/script.js
@@ -3,7 +3,7 @@
         input.focus();
         input.setSelectionRange(selectionStart, selectionEnd);
       } else if (input.createTextRange) {
-        var range = input.createTextRange();
+        let range = input.createTextRange();
         range.collapse(true);
         range.moveEnd('character', selectionEnd);
         range.moveStart('character', selectionStart);
